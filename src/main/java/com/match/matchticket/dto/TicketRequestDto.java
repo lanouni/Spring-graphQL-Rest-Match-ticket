ed50d81c604379592d032342d045f294c594bd7a
SOUHAYLLANOUNI <souhayl_lanouni@emsi-edu.ma>
2022-12-21 22:31:11 +0100

    first commit

---
+++ src/main/java/com/match/matchticket/dto/TicketRequestDto.java
@@ -0,0 +1,13 @@
+package com.match.matchticket.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TicketRequestDto {
+    private Float prix;
+}
